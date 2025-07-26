be7995306211f7a8d64fc674bb4b2f85f99f68c7
Sandeep Kumar <skumar131431@gmail.com>
2025-07-26 08:41:56 +0530

    backend

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express();
+require('dotenv').config();
+const main =  require('./src/config/db')
+const cookieParser =  require('cookie-parser');
+const authRouter = require("./src/routes/userAuth");
+const redisClient = require('./src/config/redis');
+const problemRouter = require("./src/routes/problemCreator");
+const submitRouter = require("./src/routes/submit")
+const cors = require('cors')
+const aiRouter = require("./src/routes/aiChatting")
+const videoRouter = require("./src/routes/videoCreator");
+
+
+app.use(cors({
+    // origin: 'http://localhost:5173',
+    origin: "*",
+    credentials: true 
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use('/user',authRouter);
+app.use('/problem',problemRouter);
+app.use('/ai',aiRouter);
+app.use("/video",videoRouter);
+app.use('/submission',submitRouter);
+
+
+
+
+
+const InitalizeConnection = async ()=>{
+    try{
+
+        await Promise.all([main(),redisClient.connect()]);
+        console.log("DB Connected");
+        
+        app.listen(process.env.PORT, ()=>{
+            console.log("Server listening at port number: "+ process.env.PORT);
+        })
+
+    }
+    catch(err){
+        console.log("Error: "+err);
+    }
+}
+
+
+InitalizeConnection();
+//redisClient.connect()
+
+
+
